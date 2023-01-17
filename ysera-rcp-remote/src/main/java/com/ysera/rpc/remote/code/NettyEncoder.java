02d73fcb9c98ecf3e1327bbff2735462a7a710d3
hanzhihua <zhihua9687@163.com>
2023-01-17 15:31:39 +0800

    添加项目结构

---
+++ ysera-rcp-remote/src/main/java/com/ysera/rpc/remote/code/NettyEncoder.java
@@ -0,0 +1,9 @@
+package com.ysera.rpc.remote.code;
+
+/**
+ * @author admin
+ * @ClassName NettyEncoder.java
+ * @createTime 2023年01月17日 15:06:00
+ */
+public class NettyEncoder {
+}
---
+++ ysera-rcp-remote/src/main/java/com/ysera/rpc/remote/command/YseraMessage.java
@@ -0,0 +1,9 @@
+package com.ysera.rpc.remote.command;
+
+/**
+ * @author admin
+ * @ClassName YseraMessage.java
+ * @createTime 2023年01月17日 15:08:00
+ */
+public class YseraMessage {
+}
